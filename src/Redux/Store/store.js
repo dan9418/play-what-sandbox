ca6652f4ebe42ede2b55da2b638a2f9af1d451be
Dan Bednarczyk <dpbednarczyk@gmail.com>
2019-12-14 15:19:01 -0800

    removed redux

--- src/Redux/Store/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, combineReducers } from 'redux';
-import { reducers } from '../Reducers/reducers';
-import { devToolsEnhancer } from 'redux-devtools-extension';
-
-export const store = createStore(
-    reducers,
-    {},
-    devToolsEnhancer({ realtime: true })
-);
