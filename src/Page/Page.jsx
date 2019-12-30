b8f0ad17633fbb914705f28a987dc3926b66f809
Dan Bednarczyk <dpbednarczyk@gmail.com>
2019-12-30 02:38:54 -0800

    clean up inputs

--- src/Page/Page.jsx
+++ src/Page/Page.jsx
@@ -5,26 +5,79 @@ import Section from './Section/Section';
 import { Theory, Strategies, Utils } from 'play-what';
 import { Fretboard, Keyboard, Inputs, ViewController, ChordAnalysis, ConceptBlock, NoteTable } from 'play-what-react-viewers';
 
+const CONCEPT_TYPES = [
+    {
+        id: 'intervalPair',
+        name: 'Interval Pair',
+        presets: Object.values(Theory.Presets.INTERVAL_PAIR)
+    },
+    {
+        id: 'chord',
+        name: 'Chord',
+        presets: Object.values(Theory.Presets.CHORD)
+    },
+    {
+        id: 'scale',
+        name: 'Scale',
+        presets: Object.values(Theory.Presets.SCALE)
+    },
+    {
+        id: 'mode',
+        name: 'Mode',
+        presets: Object.values(Theory.Presets.MODE)
+    }
+];
+
 export default class Page extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             keyCenter: new Theory.KeyCenter(Theory.Constants.TONIC.C, Theory.Constants.ACCIDENTAL.Natural, 4),
+            conceptType: CONCEPT_TYPES[0],
             concept: Theory.Presets.SCALE.MajorPentatonic,
             viewer: null
         }
     }
 
+    getKeyCenterInputs() {
+        let { keyCenter } = this.state;
+        let setKeyCenter = (tonic, accidental, octave) => this.setState({ keyCenter: new Theory.KeyCenter(tonic, accidental, octave) })
+        return (
+            <Section header='Key Center'>
+                <Inputs.TonicInput value={keyCenter.tonic} setValue={tonic => setKeyCenter(tonic, keyCenter.accidental, keyCenter.octave)} />
+                <Inputs.AccidentalInput value={keyCenter.accidental} setValue={accidental => setkeyCenter(keyCenter.tonic, accidental, keyCenter.octave)} />
+                <Inputs.NumericInput value={keyCenter.octave} setValue={octave => setkeyCenter(keyCenter.tonic, keyCenter.accidental, octave)} />
+            </Section>
+        );
+    }
+
+    getConceptInputs() {
+        let { conceptType, concept } = this.state;
+        return (
+            <Section header='Concept'>
+                <Inputs.DropdownInput
+                    data={CONCEPT_TYPES}
+                    value={conceptType}
+                    setValue={type => this.setState({ conceptType: type })}
+                />
+                <Inputs.DropdownInput
+                    data={conceptType.presets}
+                    value={concept}
+                    setValue={preset => this.setState({ concept: preset })}
+                />
+            </Section>
+        );
+    }
+
     render() {
         return (
             <div className='play-what-sample'>
+
                 <div className='header'>Inputs</div>
-                <Section header='Key Center'>
-                    <Inputs.KeyCenter value={this.state.keyCenter} setValue={value => this.setState({ keyCenter: value })} />
-                </Section>
-                <Section header='Concept'>
-                    <Inputs.Concept value={this.state.concept} setValue={value => this.setState({ concept: value })} />
-                </Section>
+
+                {this.getKeyCenterInputs()}
+
+                {this.getConceptInputs()}
 
                 <div className='header'>Outputs</div>
 
@@ -35,7 +88,7 @@ export default class Page extends React.Component {
                 <Section header='Note Data'>
                     <NoteTable {...this.state} />
                 </Section>
-               
+
                 <Section header='Chord Analysis'>
                     <ChordAnalysis {...this.state} />
                 </Section>
