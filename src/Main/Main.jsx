8b91061906925120856851b03b3df1af6ed04284
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-02-28 18:42:06 -0800

    top nav

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './Main.css';
+import Common from './Common/_module';
 import { Fretboard } from 'play-what-react-viewers';
 
 const Main = () => {
     return (
         <div className='controller-manager'>
-            <div className="top-nav">Play What</div>
+            <div className="top-nav">
+                <div className="link"><Common.Icons.GitHub/></div>
+                <div className="app-title">Play What</div>
+                <div className="menu"><Common.Icons.Menu/></div>
+            </div>
             <div className="stage">
                 <Fretboard.Controller />
             </div>
