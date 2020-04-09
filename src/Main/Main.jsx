7ee62c82e72d1624b0299ae82db56dee5a91df2d
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-04-08 19:05:42 -0700

    simplify

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -5,36 +5,7 @@ import Viewers from 'play-what-react-viewers';
 import PW from 'play-what';
 import * as Songs from './Songs';
 
-const Fretboard = Viewers.v1.Fretboard;
-const NumericInput = Viewers.v1.Inputs.NumericInput;
-
-const FRET_LOW = 1;
-const FRET_HIGH = 7;
-
-const parseJsong = (sections) => {
-    let viewers = [];
-    for (let i = 0; i < sections.length; i++) {
-        const s = sections[i];
-        //const intervals = s.intervals.map(ivl => typeof ivl === 'object' ? ivl : PW.Constants.INTERVAL[ivl]);
-        //const concept = new PW.Concept('', '', intervals);
-        const reps = s.repeat ? s.repeat : 1;
-        const instances = [...Array(reps)].map((e, r) => (
-            <Fretboard.Controller
-                key={i + '-' + r}
-                fretLow={typeof s.fretLow !== 'undefined' ? s.fretLow : FRET_LOW}
-                fretHigh={typeof s.fretHigh !== 'undefined' ? s.fretHigh : FRET_HIGH}
-                keyCenter={new PW.KeyCenter(s.keyCenter)}
-                concept={s.concept}
-                colorStrategy={s.colorStrategy}
-                labelStrategy={s.labelStrategy}
-                actionStrategy={s.actionStrategy}
-                mapStrategy={s.mapStrategy}
-            />
-        ));
-        viewers = viewers.concat(instances);
-    }
-    return viewers;
-}
+const Fretboard = Viewers.v2.Fretboard;
 
 const TEST = {
     strategy: 'NOTES_FROM_INTERVALS',
@@ -51,11 +22,6 @@ const TEST = {
 
 const Main = () => {
 
-    const song = Songs.G_MAJOR_SCALE.sections;
-
-    const [x, setX] = useState(2);
-    const [y, setY] = useState(4);
-
     return (
         <div className='controller-manager'>
             <div className="top-nav">
@@ -64,19 +30,14 @@ const Main = () => {
                 <div className="menu"><Common.Icons.Menu /></div>
             </div>
             <div className="stage-controller">
-                <div className="grid-input-container">
-                    <div className="text">Grid:</div>
-                    <NumericInput value={x} setValue={setX} />
-                    <div className="text">x</div>
-                    <NumericInput value={y} setValue={setY} />
-                </div>
+                Name
             </div>
             <div className="stage">
                 <pre>
                     {JSON.stringify(PW.v2.parse(TEST), null, '\t')}
                 </pre>
-                <div className="grid" style={{ gridTemplateColumns: `repeat(${x}, 1fr)`, gridTemplateRows: `repeat(${y}, 1fr)` }}>
-                    {parseJsong(song)}
+                <div>
+                    {Fretboard.Api.fromConfig()}
                 </div>
             </div>
         </div>
