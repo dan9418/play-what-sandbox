4dae714e98f35966fd3512ac65b7714fa8dd09c2
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-04-22 21:24:21 -0700

    styles

--- src/Main/HarmonicSeries/HarmonicSeries.jsx
+++ src/Main/HarmonicSeries/HarmonicSeries.jsx
@@ -2,15 +2,48 @@ import React, { useState } from 'react';
 import './HarmonicSeries.css';
 import PW from 'play-what';
 
+const MAX_PRIME = 23;
+
+const PRIMES = {
+    ['1']: 'black',
+    ['2']: 'red',
+    ['3']: 'orange',
+    ['5']: 'yellow',
+    ['7']: 'green',
+    ['11']: 'blue',
+    ['13']: 'indigo',
+    ['17']: 'purple',
+    ['19']: 'darkblue',
+    ['23']: 'darkgreen'
+}
+
 
 const Overtone = ({ n }) => {
-    const color = (n * 1000).toString(16);
+    //const color = (n * 1000).toString(16);
     //const color = Math.floor(Math.random() * parseInt('aaaaaa', 16)).toString(16);
-    console.log(color);
+    //console.log(color);
+    let color = '#ddd';
+    if(PRIMES[n]) {
+        color = PRIMES[n];
+    }
+    else {
+        let x = n;
+        while(x >= 1) {
+            x = x / 2;
+            if(PRIMES[x]) {
+                color = PRIMES[x];
+                break;
+            }
+        }
+    }
 
     return (
-        <div className="overtone" style={{gridTemplateColumns: `repeat(${n}, 1fr)`}}>
-            {[...Array(n)].map((e, i) => <div className="n" key={n} style={{ borderRight: `1px solid #${color}` }} />)}
+        <div className="overtone" style={{ gridTemplateColumns: `repeat(${n}, 1fr)` }}>
+            {[...Array(n)].map((e, i) => (
+                <div className="n" key={n} >
+                    <div className="overtone-line" style={{ backgroundColor: color }} />
+                </div>
+            ))}
         </div>
     );
 };
@@ -18,7 +51,7 @@ const Overtone = ({ n }) => {
 const HarmonicSeries = props => {
     return (
         <div className="harmonic-series">
-            {[...Array(props.n)].map((e, i) => <Overtone n={i + 1} />)}
+            {[...Array(props.n)].map((e, i) => <Overtone n={props.n - i - 1} />)}
         </div>
     );
 };
