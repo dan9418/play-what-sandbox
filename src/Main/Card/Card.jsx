2000b02b51fde0603a3ffa19cea3e598e589fc4b
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-04-29 19:48:33 -0700

    toggle cards

--- src/Main/Card/Card.jsx
+++ src/Main/Card/Card.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Card.css';
 
-const Card = ({ title, children }) => {
+const Card = ({ title, children, defaultOpen }) => {
+    const [open, setOpen] = useState(defaultOpen || false);
+
     return (
-        <div className="card">
-            <div className="title">{title}</div>
-            <div className="content">{children}</div>
+        <div className={`card ${open ? 'open' : ''}`}>
+            <div className="title" onClick={() => setOpen(!open)}>{title}</div>
+            {open && <div className="content">{children}</div>}
         </div>
     )
 }
