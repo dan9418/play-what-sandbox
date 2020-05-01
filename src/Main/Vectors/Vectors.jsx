3a7784c63a8e110f0a28fc3f28feba8d5c9c06d8
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-04-30 20:35:28 -0700

    cleanup

--- src/Main/Vectors/Vectors.jsx
+++ src/Main/Vectors/Vectors.jsx
@@ -12,10 +12,10 @@ const Presets = PW.v2.PresetsNew;
 const MAX_VECTOR = Constants.MAX_VECTOR;
 const ORIGIN = { p: 0, d: 2 };
 const VECTORS = [
-    { p: 0, d: 0 },
-    { p: 3, d: 2 },
-    { p: 6, d: 4 },
-    { p: 9, d: 6 }
+    { d: 0, p: 0 },
+    { d: 2, p: 4 },
+    { d: 4, p: 7 },
+    { d: 6, p: 11 }
 ];
 
 
@@ -27,18 +27,31 @@ const Resultant = props => {
     );
 };
 
-const Vector = props => {
+const VectorInput = props => {
     const { p, d } = props.value;
-    const data = props.point ? Presets.KEY_CENTERS : Presets.INTERVALS;
-    const interval = data.find(i => i.p === p && i.d === d);
-    const label = interval ? interval.id : '';
     return (
         <div className='vector-input'>
-            <label>p: </label>
-            <input type='number' value={p} onChange={e => props.setValue({ p: parseInt(e.target.value), d: d })} />
-            <label>d: </label>
-            <input type='number' value={d} onChange={e => props.setValue({ p: p, d: parseInt(e.target.value) })} />
-            <label>{label}</label>
+            <input type='number' value={d} onChange={e => props.setValue({ p: parseInt(e.target.value), d: d })} />
+            <input type='number' value={p} onChange={e => props.setValue({ p: p, d: parseInt(e.target.value) })} />
+        </div>
+    );
+};
+
+const VectorLabel = ({ value }) => {
+    return (
+        <div className='vector-label'>
+            {Theory.getIntervalName(value)}
+        </div>
+    );
+};
+
+const VectorRow = ({ vector, origin, max, setValue }) => {
+    const resultant = Theory.addVectors(origin, vector, max)
+    console.log(vector);
+    return (
+        <div className='vector-row'>
+            <VectorInput value={vector} setValue={setValue} />
+            <VectorLabel value={vector} max={max} />
         </div>
     );
 };
@@ -48,38 +61,30 @@ const Vectors = () => {
     const [origin, setOrigin] = useState(ORIGIN);
     const [vectors, setVectors] = useState(VECTORS);
 
-    const resultants = Theory.addVectorsBatch(origin, vectors, max);
-    
-    const maxInput = <Vector value={max} setValue={setMax} />
-    const originInput = <Vector value={origin} setValue={setOrigin} point />
-    const vectorInputs = resultants.map((v, i) => <Vector key={i} value={v} setValue={x => setVectors([...vectors.slice(0, i), x, ...(vectors.slice(i + 1))])} />);
-    const resultantOutputs = resultants.map((r, i) => <Resultant key={i} value={r} max={max} />);
+    const maxInput = <VectorInput value={max} setValue={setMax} />
+    const originInput = <VectorInput value={origin} setValue={setOrigin} point />
+    const rows = vectors.map((v, i) => (
+        <VectorRow key={i} vector={v} origin={origin} max={max} setValue={x => setVectors([...vectors.slice(0, i), x, ...(vectors.slice(i + 1))])} />
+    ));
 
-    const Add = () => <input type='button' value='Add' onClick={() => setVectors([...vectors, origin])} />
+    // const Add = () => <input type='button' value='Add' onClick={() => setVectors([...vectors, origin])} />
 
     return (
         <div className='vectors'>
             <div className='left'>
                 <div className='origin-input'>
-                    <label>Max Vector</label>
-                    {maxInput}
-                </div>
-                <div className='origin-input'>
-                    <label>Origin</label>
                     {originInput}
                 </div>
-                <div className='vectors-input'>
-                    <label>Vectors</label>
-                    {vectorInputs}
-                    <Add />
-                </div>
-                <div className='resultants'>
-                    <label>Resultants</label>
-                    {resultantOutputs}
+                <div className='vector-rows'>
+                    {rows}
                 </div>
             </div>
             <div className='right'>
                 <Graph origin={origin} vectors={vectors} x={max.p} y={max.d} />
+                <div className='max-input'>
+                    <label>Max Vector</label>
+                    {maxInput}
+                </div>
             </div>
         </div>
     );
