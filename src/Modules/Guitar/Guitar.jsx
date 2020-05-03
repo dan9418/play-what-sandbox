46487d6babca28ff88abf6fc4d46816b49b8c981
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-05-03 15:49:53 -0700

    working

--- src/Modules/Guitar/Guitar.jsx
+++ src/Modules/Guitar/Guitar.jsx
@@ -8,9 +8,10 @@ import Viewers from 'play-what-react-viewers';
 
 const Fretboard = Viewers.v2.Fretboard.Viewer;
 
-const NOTES = Theory.addVectorsBatch(Presets.KEY_CENTERS.G, Presets.SCALE.NaturalMinor.intervals);
+const NOTES = Theory.addVectorsBatch(Presets.KEY_CENTERS.A, Presets.SCALE.NaturalMinor.intervals);
 
 const Guitar = () => {
+    console.log('notes', NOTES);
     return (
         <div className="guitar">
             <Fretboard 
