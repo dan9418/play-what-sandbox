0548e8867662af2457615918caaeaa2cad7da003
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-07-01 20:35:17 -0700

    blank canvas

--- src/PlaybackControls/PlaybackControls.jsx
+++ src/PlaybackControls/PlaybackControls.jsx
@@ -1,26 +1,20 @@
 import React, { useState } from 'react';
 import PW from 'play-what';
 
-import ConceptInput from '../../UI/ConceptInput/ConceptInput';
-import ScalarInput from '../../UI/ScalarInput/ScalerInput';
-import SwitchInput from '../../UI/SwitchInput/SwitchInput';
-import InputBlock from '../../UI/InputBlock/InputBlock';
-import LabeledInput from '../../UI/LabeledInput/LabeledInput';
-import useNoteContext from '../../Utils/NoteContext';
-import ButtonInput from '../../UI/ButtonInput/ButtonInput';
+import useNoteContext from '../Common/NoteContext';
 
 import './PlaybackControls.css';
+import ButtonInput from 'play-what-react-viewers/src/UI/ButtonInput/ButtonInput';
+import ScalarInput from 'play-what-react-viewers/src/UI/ScalarInput/ScalerInput';
 
 const PlaybackControls = () => {
     const { playing, togglePlay, tempo, setTempo, beatIndex } = useNoteContext();
     return (
-        <InputBlock>
-            <div className='playback-controls'>
-                <ButtonInput className='pw-secondary' onClick={togglePlay}>{playing ? 'Pause' : 'Play'}</ButtonInput>
-                <ScalarInput value={tempo} setValue={setTempo} />
-                <div className={`blinker ${beatIndex % 2 === 0 ? 'pulse' : ''}`} />
-            </div>
-        </InputBlock>
+        <div className='playback-controls'>
+            <ButtonInput className='pw-secondary' onClick={togglePlay}>{playing ? 'Pause' : 'Play'}</ButtonInput>
+            <ScalarInput value={tempo} setValue={setTempo} />
+            <div className={`blinker ${beatIndex % 2 === 0 ? 'pulse' : ''}`} />
+        </div>
     );
 }
 
--- src/Stage/Stage.jsx
+++ src/Stage/Stage.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import './Stage.css';
 
+import PlaybackControls from '../PlaybackControls/PlaybackControls';
+
 import MasterSelector from './MasterSelector';
 
 const Stage = () => {
@@ -8,8 +10,10 @@ const Stage = () => {
         <div className="stage pw-light">
             <h1>Play What?</h1>
             <p>Play What is a JavaScript library for musical concepts. More info TBA...</p>
+            <hr />
             <MasterSelector />
             <hr />
+            <PlaybackControls />
         </div>
     );
 }
