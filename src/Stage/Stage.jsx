2a9383efb18d75fcfc978d99961e8ad9c55c2663
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-07-02 17:33:17 -0700

    center col

--- src/Stage/Stage.jsx
+++ src/Stage/Stage.jsx
@@ -7,13 +7,12 @@ import MasterSelector from './MasterSelector';
 
 const Stage = () => {
     return (
-        <div className="stage pw-light">
-            <h1>Play What?</h1>
-            <p>Play What is a JavaScript library for musical concepts. More info TBA...</p>
-            <hr />
-            <MasterSelector />
-            <hr />
-            <PlaybackControls />
+        <div className="stage pw-medium">
+            <div className="center pw-light">
+                <MasterSelector />
+                <hr />
+                <PlaybackControls />
+            </div>
         </div>
     );
 }
