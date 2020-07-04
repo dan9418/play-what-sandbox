a97ad5e0b59166b053dae8872bc0a43a28bcc045
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-07-03 18:41:17 -0700

    new working state mgmt

--- src/PlaybackControls/PlaybackControls.jsx
+++ src/PlaybackControls/PlaybackControls.jsx
@@ -5,84 +5,80 @@ import './PlaybackControls.css';
 import ButtonInput from 'play-what-react-viewers/src/UI/ButtonInput/ButtonInput';
 import ScalarInput from 'play-what-react-viewers/src/UI/ScalarInput/ScalerInput';
 import { useRecoilState } from 'recoil';
-import { positionState } from '../Stage/State';
+import { positionState, sourceState, inputModeState } from '../Stage/State';
 import AUTUMN_LEAVES from '../Common/AutumnLeaves';
+import { useRecoilValue } from 'recoil';
 
 const DEFAULT_NOTE = { a: [0, 0], B: [[0, 0]] };
 const DEFAULT_SONG = AUTUMN_LEAVES;
 const DEFAULT_TEMPO = 120;
 const NOP = () => null;
 
-const DEFAULT_NOTE_CONTEXT = {
-    // Full data
-    song: { sections: [{ rows: [{ cols: [DEFAULT_NOTE] }] }] },
-    // State
-    state: [0, 0, 0, 0, 0],
-    // State helpers
-    position: [0, 0, 0],
-    setPosition: NOP,
-    sectionIndex: 0,
-    section: { rows: [] },
-    rowIndex: 0,
-    row: { cols: [] },
-    colIndex: 0,
-    col: DEFAULT_NOTE,
-    note: DEFAULT_NOTE,
-    setNote: NOP,
-    nextNote: DEFAULT_NOTE,
-    // Playback
-    tempo: DEFAULT_TEMPO,
-    setTempo: NOP,
-    playing: NOP,
-    togglePlay: NOP
-};
-
 const useToggle = (initValue = false) => {
     const [value, setValue] = useState(initValue);
     return [value, () => setValue(!value)];
 };
 
-const getNextPosition = (song, [s, r, c]) => {
-    const isLastSection = s === song.sections.length - 1;
-    const isLastRow = r === song.sections[s].rows.length - 1;
-    const isLastCol = c === song.sections[s].rows[r].cols.length - 1;
-    if (isLastCol) {
-        if (isLastRow) {
-            if (isLastSection) {
-                return [0, 0, 0];
+const getNextPosition = (source, position) => {
+    const inputMode = useRecoilValue(inputModeState);
+    switch (inputMode.id) {
+        case 'concept':
+            return null;
+        case 'progression':
+            const isLast = position === source.cols.length - 1;
+            return isLast ? 0 : position + 1;
+        case 'chart':
+            const [s, r, c] = position;
+            const isLastSection = s === source.sections.length - 1;
+            const isLastRow = r === source.sections[s].rows.length - 1;
+            const isLastCol = c === source.sections[s].rows[r].cols.length - 1;
+            if (isLastCol) {
+                if (isLastRow) {
+                    if (isLastSection) {
+                        return [0, 0, 0];
+                    }
+                    return [s + 1, 0, 0];
+                }
+                return [s, r + 1, 0];
             }
-            return [s + 1, 0, 0];
-        }
-        return [s, r + 1, 0];
+            return [s, r, c + 1];
     }
-    return [s, r, c + 1];
+
 };
 
-const getCol = (song, s, r, c) => {
-    const section = song.sections[s];
-    const row = section.rows[r];
-    const col = row.cols[c];
-    return col;
+const getCol = (source, position) => {
+    const inputMode = useRecoilValue(inputModeState);
+    switch (inputMode.id) {
+        case 'concept':
+            return source;
+        case 'progression':
+            return source.cols[position];
+        case 'chart':
+            const [s, r, c] = position;
+            const section = source.sections[s];
+            const row = section.rows[r];
+            const col = row.cols[c];
+            return col;
+    }
 }
 
 const PlaybackControls = () => {
 
     const [position, setPosition] = useRecoilState(positionState);
-    const [sectionIndex, rowIndex, colIndex] = position;
 
     // Full data
-    const [song, setSong] = useState(DEFAULT_SONG);
+    const source = useRecoilValue(sourceState);
     // State (Compound)
-    const [state, setState] = useState([0, getCol(song, 0, 0, 0).t]);
+    const [state, setState] = useState([0, getCol(source, position).t]);
     // State helpers
     const [beatIndex, remBeats] = state;
-    const section = song.sections[sectionIndex];
-    const row = section.rows[rowIndex];
-    const col = row.cols[colIndex];
-    const nextPosition = getNextPosition(song, position);
+    const col = getCol(source, position);
+    const nextPosition = getNextPosition(source, position);
+    const nextCol = getCol(source, nextPosition);
     // Playback
     const [tempo, setTempo] = useState(DEFAULT_TEMPO);
     const [playing, togglePlay] = useToggle(false);
+
 
     if (!playing) {
         PW.Sound.stopNotes();
@@ -103,8 +99,6 @@ const PlaybackControls = () => {
             console.log(beatIndex, remBeats, '-', state);
         }
         else if (remBeats === 1) {
-            const nextPosition = getNextPosition(song, position);
-            const nextCol = getCol(song, ...nextPosition);
             setTimeout(() => {
                 setPosition(nextPosition);
                 setState([beatIndex + 1, nextCol.t]);
@@ -112,14 +106,6 @@ const PlaybackControls = () => {
             console.log(beatIndex, remBeats, '>', state);
         }
     }
-
-    const setNote = note => {
-        const songCopy = { ...song };
-        const col = getCol(songCopy, ...position);
-        col = note;
-        setSong(songCopy);
-    }
-
 
     return (
         <div className='playback-controls'>
