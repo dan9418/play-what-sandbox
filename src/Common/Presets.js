8b938480e6cdedaa3bb9f78828119a7d523bca98
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-07-05 19:28:15 -0700

    add blue bossa

--- src/Common/Presets.js
+++ src/Common/Presets.js
@@ -520,5 +520,107 @@ export const CHARTS = [
                     ]
                 }
             ]
+    },
+    {
+        id: 'blueBossa',
+        name: 'Blue Bossa',
+        sections: [
+            {
+                name: 'A',
+                rows: [
+                    {
+                        cols: [
+                            {
+                                a: PW.Presets.KEY_CENTERS.C,
+                                B: PW.Presets.CHORD.Min7.intervals,
+                                t: 8
+                            },
+                            {
+                                a: PW.Presets.KEY_CENTERS.F,
+                                B: PW.Presets.CHORD.Min6.intervals,
+                                t: 4
+                            },
+                            {
+                                a: PW.Presets.KEY_CENTERS.Bb,
+                                B: PW.Presets.CHORD.Dom7.intervals,
+                                t: 4
+                            }
+                        ]
+                    },
+                    {
+                        cols: [
+                            {
+                                a: PW.Presets.KEY_CENTERS.D,
+                                B: PW.Presets.CHORD.HalfDim7.intervals,
+                                t: 4
+                            },
+                            {
+                                a: PW.Presets.KEY_CENTERS.G,
+                                B: PW.Presets.CHORD.Dom7.intervals,
+                                t: 4
+                            },
+                            {
+                                a: PW.Presets.KEY_CENTERS.C,
+                                B: PW.Presets.CHORD.Min7.intervals,
+                                t: 8
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                name: 'B',
+                rows: [
+                    {
+                        cols: [
+                            {
+                                a: PW.Presets.KEY_CENTERS.Eb,
+                                B: PW.Presets.CHORD.Min7.intervals,
+                                t: 4
+                            },
+                            {
+                                a: PW.Presets.KEY_CENTERS.Ab,
+                                B: PW.Presets.CHORD.Dom7.intervals,
+                                t: 4
+                            },
+                            {
+                                a: PW.Presets.KEY_CENTERS.Db,
+                                B: PW.Presets.CHORD.Maj7.intervals,
+                                t: 8
+                            }
+                        ]
+                    },
+                    {
+                        cols: [
+                            {
+                                a: PW.Presets.KEY_CENTERS.D,
+                                B: PW.Presets.CHORD.HalfDim7.intervals,
+                                t: 4
+                            },
+                            {
+                                a: PW.Presets.KEY_CENTERS.G,
+                                B: PW.Presets.CHORD.Dom7.intervals,
+                                t: 4
+                            },
+                            {
+                                a: PW.Presets.KEY_CENTERS.C,
+                                B: PW.Presets.CHORD.Min7.intervals,
+                                t: 4
+                            },
+                            {
+                                a: PW.Presets.KEY_CENTERS.D,
+                                B: PW.Presets.CHORD.HalfDim7.intervals,
+                                t: 2
+                            },
+                            {
+                                a: PW.Presets.KEY_CENTERS.G,
+                                B: PW.Presets.CHORD.Dom7.intervals,
+                                t: 2
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
     }
 ];
