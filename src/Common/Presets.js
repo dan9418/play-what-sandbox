2d14de2c6dcd01b22596ab4b88c515760ebf9553
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-07-05 19:44:44 -0700

    build

--- src/Common/Presets.js
+++ src/Common/Presets.js
@@ -622,5 +622,228 @@ export const CHARTS = [
                 ]
             }
         ]
+    },
+    {
+        id: 'blueWorld',
+        name: 'It\'s A Blue World',
+        sections: [
+            {
+                name: 'A',
+                rows: [
+                    {
+                        cols: [
+                            {
+                                a: PW.Presets.KEY_CENTERS.C,
+                                B: PW.Presets.CHORD.Min7.intervals,
+                                t: 4
+                            },
+                            {
+                                a: PW.Presets.KEY_CENTERS.F,
+                                B: PW.Presets.CHORD.Dom7.intervals,
+                                t: 4
+                            },
+                            {
+                                a: PW.Presets.KEY_CENTERS.Bb,
+                                B: PW.Presets.CHORD.Maj7.intervals,
+                                t: 8
+                            }
+                        ]
+                    },
+                    {
+                        cols: [
+                            {
+                                a: PW.Presets.KEY_CENTERS.F,
+                                B: PW.Presets.CHORD.Min7.intervals,
+                                t: 4
+                            },
+                            {
+                                a: PW.Presets.KEY_CENTERS.Bb,
+                                B: PW.Presets.CHORD.Dom7.intervals,
+                                t: 4
+                            },
+                            {
+                                a: PW.Presets.KEY_CENTERS.Eb,
+                                B: PW.Presets.CHORD.Dom7.intervals,
+                                t: 8
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                name: 'B',
+                rows: [
+                    {
+                        cols: [
+                            {
+                                a: PW.Presets.KEY_CENTERS.Eb,
+                                B: PW.Presets.CHORD.Maj7.intervals,
+                                t: 4
+                            },
+                            {
+                                a: PW.Presets.KEY_CENTERS.Eb,
+                                B: PW.Presets.CHORD.Min7.intervals,
+                                t: 2
+                            },
+                            {
+                                a: PW.Presets.KEY_CENTERS.F,
+                                B: PW.Presets.CHORD.Dom7.intervals,
+                                t: 2
+                            },
+                            {
+                                a: PW.Presets.KEY_CENTERS.Bb,
+                                B: PW.Presets.CHORD.Maj7.intervals,
+                                t: 4
+                            },
+                            {
+                                a: PW.Presets.KEY_CENTERS.Bb,
+                                B: PW.Presets.CHORD.Dom7.intervals,
+                                t: 4
+                            }
+                        ]
+                    },
+                    {
+                        cols: [
+                            {
+                                a: PW.Presets.KEY_CENTERS.Eb,
+                                B: PW.Presets.CHORD.Min7.intervals,
+                                t: 4
+                            },
+                            {
+                                a: PW.Presets.KEY_CENTERS.Gb,
+                                B: PW.Presets.CHORD.Maj7.intervals,
+                                t: 4
+                            },
+                            {
+                                a: PW.Presets.KEY_CENTERS.F,
+                                B: PW.Presets.CHORD.Maj7.intervals,
+                                t: 2
+                            },
+                            {
+                                a: PW.Presets.KEY_CENTERS.Gb,
+                                B: PW.Presets.CHORD.Maj7.intervals,
+                                t: 2
+                            },
+                            {
+                                a: PW.Presets.KEY_CENTERS.F,
+                                B: PW.Presets.CHORD.Maj7.intervals,
+                                t: 4
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                name: 'A',
+                rows: [
+                    {
+                        cols: [
+                            {
+                                a: PW.Presets.KEY_CENTERS.C,
+                                B: PW.Presets.CHORD.Min7.intervals,
+                                t: 4
+                            },
+                            {
+                                a: PW.Presets.KEY_CENTERS.F,
+                                B: PW.Presets.CHORD.Dom7.intervals,
+                                t: 4
+                            },
+                            {
+                                a: PW.Presets.KEY_CENTERS.Bb,
+                                B: PW.Presets.CHORD.Maj7.intervals,
+                                t: 8
+                            }
+                        ]
+                    },
+                    {
+                        cols: [
+                            {
+                                a: PW.Presets.KEY_CENTERS.F,
+                                B: PW.Presets.CHORD.Min7.intervals,
+                                t: 4
+                            },
+                            {
+                                a: PW.Presets.KEY_CENTERS.Bb,
+                                B: PW.Presets.CHORD.Dom7.intervals,
+                                t: 4
+                            },
+                            {
+                                a: PW.Presets.KEY_CENTERS.Eb,
+                                B: PW.Presets.CHORD.Dom7.intervals,
+                                t: 8
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                name: 'C',
+                rows: [
+                    {
+                        cols: [
+                            {
+                                a: PW.Presets.KEY_CENTERS.Eb,
+                                B: PW.Presets.CHORD.Maj7.intervals,
+                                t: 4
+                            },
+                            {
+                                a: PW.Presets.KEY_CENTERS.Eb,
+                                B: PW.Presets.CHORD.Min7.intervals,
+                                t: 2
+                            },
+                            {
+                                a: PW.Presets.KEY_CENTERS.Ab,
+                                B: PW.Presets.CHORD.Maj7.intervals,
+                                t: 2
+                            },
+                            {
+                                a: PW.Presets.KEY_CENTERS.Bb,
+                                B: PW.Presets.CHORD.Maj7.intervals,
+                                t: 2
+                            },
+                            {
+                                a: PW.Presets.KEY_CENTERS.C,
+                                B: PW.Presets.CHORD.Min7.intervals,
+                                t: 2
+                            },
+                            {
+                                a: PW.Presets.KEY_CENTERS.D,
+                                B: PW.Presets.CHORD.HalfDim7.intervals,
+                                t: 2
+                            },
+                            {
+                                a: PW.Presets.KEY_CENTERS.G,
+                                B: PW.Presets.CHORD.Dom7.intervals,
+                                t: 2
+                            }
+                        ]
+                    },
+                    {
+                        cols: [
+                            {
+                                a: PW.Presets.KEY_CENTERS.C,
+                                B: PW.Presets.CHORD.Maj7.intervals,
+                                t: 4
+                            },
+                            {
+                                a: PW.Presets.KEY_CENTERS.C,
+                                B: PW.Presets.CHORD.Min7.intervals,
+                                t: 2
+                            },
+                            {
+                                a: PW.Presets.KEY_CENTERS.F,
+                                B: PW.Presets.CHORD.Dom7.intervals,
+                                t: 2
+                            },
+                            {
+                                a: PW.Presets.KEY_CENTERS.Bb,
+                                B: PW.Presets.CHORD.Maj7.intervals,
+                                t: 8
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
     }
 ];
