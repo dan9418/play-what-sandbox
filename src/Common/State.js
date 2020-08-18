433a6230aef6471af19182c51b6df429575f825a
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-08-18 08:41:04 -0700

    refactor

--- src/Common/State.js
+++ src/Common/State.js
@@ -2,56 +2,52 @@ import PW from 'play-what';
 import { atom, selector } from 'recoil';
 
 const RAW_SOURCE = {
-    props: {
-        name: 'Test Source',
-        // 1) string - can be pw, in, or primitive
-        a: 'pw/keyCenter/presets/C/a',
-        // 1.5) array
-        B: 'pw/concept/presets/Maj/B',
-        // 2) object - attrs can be inputs or primitives (recurse on each)
-        // primitives can be number, null, or boolean - or string w/o "/"
-        a: {
-            p: 0,
-            d: 0
-        },
-        // 3) function - fn and args attrs reserved (recurse on each arg)
-        a: {
-            fn: 'pw/keyCenter/from',
-            args: {
-                preset: 'C'
-            }
-        },
-        children: [
-            {
-                props: {
-                    notes: {
-                        fn: 'pw/concept/from',
-                        args: {
-                            a: 'parent/a',
-                            B: 'parent/B'
-                        }
-                    },
-                    children: [
-                        {
-                            fn: 'pw/viewer/fretboard/create',
+    name: 'Test Source',
+    // 1) string - can be pw, in, or primitive
+    a: 'pw/keyCenter/presets/C/a',
+    // 1.5) array
+    B: 'pw/concept/presets/Maj/B',
+    // 2) object - attrs can be inputs or primitives (recurse on each)
+    // primitives can be number, null, or boolean - or string w/o "/"
+    a: {
+        p: 0,
+        d: 0
+    },
+    // 3) function - fn and args attrs reserved (recurse on each arg)
+    a: {
+        fn: 'pw/keyCenter/from',
+        args: {
+            preset: 'C'
+        }
+    },
+    children: [
+        {
+            notes: {
+                fn: 'pw/concept/from',
+                args: {
+                    a: 'parent/a',
+                    B: 'parent/B'
+                }
+            },
+            children: [
+                {
+                    fn: 'pw/viewer/fretboard/create',
+                    args: {
+                        name: 'Fretboard',
+                        fretRange: [0, 12],
+                        tuning: [16, 11, 7, 2, -3, 8], // e B G D A E
+                        labelFn: {
+                            fn: 'pw/note/label',
                             args: {
-                                name: 'Fretboard',
-                                fretRange: [0, 12],
-                                tuning: [16, 11, 7, 2, -3, 8], // e B G D A E
-                                labelFn: {
-                                    fn: 'pw/note/label',
-                                    args: {
-                                        type: 'degree',
-                                        notes: 'parent/notes'
-                                    }
-                                }
+                                type: 'degree',
+                                notes: 'parent/notes'
                             }
                         }
-                    ]
+                    }
                 }
-            }
-        ]
-    }
+            ]
+        }
+    ]
 }
 
 
