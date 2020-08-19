04b17f1d2b89597ecb2009a4fd63a7d117ac7623
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-08-18 18:58:21 -0700

    clean up source

--- src/Common/State.js
+++ src/Common/State.js
@@ -7,7 +7,34 @@ const RAW_SOURCE = {
         {
             name: 'API Constants',
             a: 'pw/keyCenter/presets/C/a',
-            B: 'pw/concept/presets/Maj/B'
+            B: 'pw/concept/presets/Maj/B',
+            children: [
+                {
+                    name: 'Generate Notes',
+                    notes: {
+                        fn: 'pw/notes/from',
+                        args: {
+                            a: 'parent/a',
+                            B: 'parent/B'
+                        }
+                    },
+                    children: [
+                        {
+                            fn: 'pw/viewer/fretboard/create',
+                            args: {
+                                name: 'Fretboard',
+                                labelFn: {
+                                    fn: 'pw/note/label',
+                                    args: {
+                                        type: 'degree',
+                                        notes: 'parent/notes'
+                                    }
+                                }
+                            }
+                        }
+                    ]
+                }
+            ]
         },
         {
             name: 'API Functions',
@@ -32,45 +59,19 @@ const RAW_SOURCE = {
             },
             B: [
                 {
-                  "d": 0,
-                  "p": 0
+                    d: 0,
+                    p: 0
                 },
                 {
-                  "d": 2,
-                  "p": 4
+                    d: 2,
+                    p: 4
                 },
                 {
-                  "d": 4,
-                  "p": 7
-                }
-              ]
-        }
-        /*{
-            notes: {
-                fn: 'pw/concept/from',
-                args: {
-                    a: 'parent/a',
-                    B: 'parent/B'
-                }
-            },
-            children: [
-                {
-                    fn: 'pw/viewer/fretboard/create',
-                    args: {
-                        name: 'Fretboard',
-                        fretRange: [0, 12],
-                        tuning: [16, 11, 7, 2, -3, 8], // e B G D A E
-                        labelFn: {
-                            fn: 'pw/note/label',
-                            args: {
-                                type: 'degree',
-                                notes: 'parent/notes'
-                            }
-                        }
-                    }
+                    d: 4,
+                    p: 7
                 }
             ]
-        }*/
+        }
     ]
 }
 
