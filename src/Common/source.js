676e834be90ecd0c358bcb93dc9f96a604f1b766
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-08-24 17:50:18 -0700

    together

--- src/Common/source.js
+++ src/Common/source.js
@@ -14,26 +14,6 @@ const RAW_SOURCE = {
         }
     },
     children: [
-        /*{
-            name: 'For Each',
-            fn: 'PW_React/Summary/forEachNote/',
-            args: {
-                concepts: AUTUMN_LEAVES.children[0].children,
-                props: {
-                    colorFn: {
-                        fn: 'PW_React/Fretboard/colorBy/',
-                        args: {
-                            type: 'degree',
-                            //scheme: null
-                        }
-                    }
-                }
-            }
-        },*/
-        {
-            component: 'h3',
-            children: ['Note Summary']
-        },
         {
             component: 'PW_React/List/component',
             props: {
@@ -48,62 +28,6 @@ const RAW_SOURCE = {
                                 //scheme: null
                             }
                         }
-                    }
-                }
-            }
-        },
-        {
-            component: 'PW_React/Chart/component',
-            props: {
-                chart: AUTUMN_LEAVES,
-                viewer: {
-                    component: 'PW_React/Summary/component',
-                    props: {
-                        colorFn: {
-                            fn: 'PW_React/Fretboard/colorBy/',
-                            args: {
-                                type: 'degree',
-                                //scheme: null
-                            }
-                        }
-                    }
-                }
-            }
-        },
-        {
-            component: 'PW_React/Summary/component',
-            props: {
-                keyCenter: 'PW/Vector/Note/Preset/C/value',
-                intervals: 'PW/Matrix/Chord/Preset/Maj/value',
-                colorFn: {
-                    fn: 'PW_React/Fretboard/colorBy/',
-                    args: {
-                        type: 'degree',
-                        //scheme: null
-                    }
-                }
-            }
-        },
-        {
-            component: 'h3',
-            children: ['Degree']
-        },
-        {
-            component: 'PW_React/Fretboard/component',
-            props: {
-                notes: 'parent/notes',
-                colorFn: {
-                    fn: 'PW_React/Fretboard/colorBy/',
-                    args: {
-                        type: 'degree',
-                        //scheme: null
-                    }
-                },
-                textFn: {
-                    fn: 'PW_React/Fretboard/textBy/',
-                    args: {
-                        type: 'degree',
-                        //scheme: null
                     }
                 }
             }
